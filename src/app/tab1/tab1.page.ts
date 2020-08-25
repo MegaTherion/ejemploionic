9a3ba35b0ba2d125c1d94ec05c97cee187192a23
Christian Gomez <christian.gomez.4@gmail.com>
2020-08-25 18:55:50 -0400

    archvos modificados

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  dado1 = 0;
+  dado2 = 0;
+  suma = 0;
+
   constructor() {}
+
+  calcularSuma() {
+    this.suma = this.dado1 + this.dado2;
+    // En php: $this->suma = $this->dado1 + $this->dado2
+  }
 
 }
