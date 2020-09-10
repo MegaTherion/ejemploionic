215bae23fa5f9ebb43c798b11af548848dda955f
Christian Gomez <christian.gomez.4@gmail.com>
2020-09-10 19:19:48 -0400

    implementando colas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  dato = '';
+  v: Array<string> = new Array<string>(5);
+  entrada: number;
+  salida: number;
+
+  constructor() {
+    this.entrada = -1;
+    this.salida = -1;
+  }
+
+  vacia(): boolean {
+    return (this.entrada === -1 && this.salida === -1);
+  }
+
+  llena(): boolean {
+    return (this.entrada === 4);
+  }
+
+  insertar(dato: string) {
+    if (this.vacia()) {
+      this.entrada = this.salida = 0;
+      this.v[this.entrada] = dato;
+    } else {
+      if (!this.llena()) {
+        this.entrada++;
+        this.v[this.entrada] = dato;
+      } else {
+        console.log('cola llena');
+      }
+    }
+  }
+
+  extraer() {
+    
+  }
+
+  ver() {
+    console.log(this.v);
+  }
 
 }
